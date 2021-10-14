b5c51962ee899566fbdf1bb974916be446b3d74e
4kpros <prosper.abouar@gmail.com>
2021-10-14 14:50:59 +0100

    update

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 require('./models/dbConfig');
 const postsRoutes = require('./controllers/postsController');
 const commentsRoutes = require('./controllers/commentsController');
+const cors = require('cors');
 const port = process.env.PORT || 5000
 
 app.use(BodyParser.json());
+app.use((cors()));
 app.use('/posts', postsRoutes);
 app.use('/comments', commentsRoutes);
 
