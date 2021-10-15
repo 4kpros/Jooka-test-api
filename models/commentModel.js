1feca8fc55ea1a1d9f6da0668f165b5348f76b75
4kpros <prosper.abouar@gmail.com>
2021-10-15 05:47:57 +0100

    Deployment

--- models/commentModel.js
+++ models/commentModel.js
@@ -2,34 +2,23 @@ const mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 var CommentSchema = new Schema({
-	post_id:String,
-	sender_mail:String,
-	content:String,
-	date:Date
+	post_id: {
+		type: Number,
+		required: true
+	},
+	sender_mail: {
+		type: String,
+		required: true
+	},
+	content: {
+		type: String,
+		required: true
+	},
+	date: {
+		type: Date,
+		default: Date.now
+	}
  });
  const CommentModel = module.exports = mongoose.model('comments', CommentSchema);  
-
-// const CommentModel = mongoose.model(
-// 	"jooka-test-api",
-// 	{
-// 		post_id: {
-// 			type: Number,
-// 			required: true
-// 		},
-// 		sender_mail: {
-// 			type: String,
-// 			required: true
-// 		},
-// 		content: {
-// 			type: String,
-// 			required: true
-// 		},
-// 		date: {
-// 			type: Date,
-// 			default: Date.now
-// 		}
-// 	},
-// 	"comments"
-// );
 
 module.exports = { CommentModel };
--- models/postModel.js
+++ models/postModel.js
@@ -2,37 +2,24 @@ const mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 var postSchema = new Schema({
-	title:String,
-	description:String,
-	date:Date,
-	sender:String,
-	thumbnail:String
+	title: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String,
+	},
+	date: {
+		type: Date,
+		default: Date.now
+	},
+	sender: {
+		type: String,
+		required: true
+	},
+	thumbnail: {
+		type: String
+	}
 });
 const PostModel = module.exports = mongoose.model('posts', postSchema);  
-
-// const PostModel = mongoose.model(
-// 	"jooka-test-api",
-// 	{
-// 		title: {
-// 			type: String,
-// 			required: true
-// 		},
-// 		description: {
-// 			type: String,
-// 		},
-// 		date: {
-// 			type: Date,
-// 			default: Date.now
-// 		},
-// 		sender: {
-// 			type: String,
-// 			required: true
-// 		},
-// 		thumbnail: {
-// 			type: String
-// 		}
-// 	},
-// 	"posts"
-// );
-
 module.exports = { PostModel };
